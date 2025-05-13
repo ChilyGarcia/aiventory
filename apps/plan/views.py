51dab0836115ced98bfe7122a2ce4aa0b5e55bfc
JuanGarcia <82130952+ChilyGarcia@users.noreply.github.com>
2025-05-13 10:32:42 -0500

    <feat>: Implemented retrieve plan by id

--- apps/plan/views.py
+++ apps/plan/views.py
@@ -4,7 +4,8 @@ from .serializers import PlanSerializer
 from rest_framework.permissions import AllowAny
 
 
-class PlanViewSet(mixins.ListModelMixin, viewsets.GenericViewSet):
+class PlanViewSet(mixins.ListModelMixin, mixins.RetrieveModelMixin,
+                  viewsets.GenericViewSet):
     permission_classes = [AllowAny]
     queryset = Plan.objects.all()
     serializer_class = PlanSerializer
